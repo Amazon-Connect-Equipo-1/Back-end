69a56f82ed98fa9ddf86d7d1d7d477541f4b12b7
IsraelSanchezM <58601226+tamagochi-descompuesto@users.noreply.github.com>
2022-05-25 21:31:49 -0500

    Update pm2.config.js
    Co-Authored-By: David Rodriguez Fragoso <42352919+A01748760@users.noreply.github.com>

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "backend",
-        command: "dist/index.js",
-        node_arg: "-r dotenv/config" 
+        script: "dist/index.js",
+        node_args: "-r dotenv/config"  
     }]
 }
